0aa51c6f408c951670ed68c66f380d6ca439087b
Axelle Ziegler <axelle-z@evidenceb.com>
2025-12-26 17:52:24 +0100

    Refacto

---
+++ src/fs.rs
@@ -0,0 +1,40 @@
+use std::{env, os::unix::fs::PermissionsExt, path::{self, Path}, str::FromStr};
+
+pub struct PathCollection{
+    paths : Vec<String>
+}
+
+impl FromStr for PathCollection{
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+       Ok(
+        PathCollection{
+         paths : s.split(":").map(|s| s.to_owned()).collect()
+        })
+    }
+}
+
+impl PathCollection {
+    pub fn build() -> Result<Self,&'static str> {
+        let Ok(path) = env::var("PATH") else {
+            return Err("Path not set");
+        };
+        PathCollection::from_str(&path)
+    }
+
+    pub fn find(&self, cmd: String) -> Option<String> {
+        for path in &self.paths {
+            let path = Path::new(&path).join(&cmd);
+            if !path.exists() {
+                continue;
+            }
+            let Ok(meta) = path.metadata() else { continue;};
+
+            if meta.permissions().mode() & 0o111 != 0 {
+                return path.to_str().map(|s| s.to_string());               
+            }            
+        }
+        None
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+pub mod fs;
+
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::{env, os::unix::fs::PermissionsExt, path::Path, str::FromStr};
+use std::str::FromStr;
+
+use crate::fs::PathCollection;
 
 enum Command{Echo,Exit,Type}
 
@@ -14,45 +18,6 @@ impl FromStr for Command {
             "type" => Ok(Command::Type),
             _ => Err("Not Found"),
         }
-    }
-}
-
-struct PathCollection{
-    paths : Vec<String>
-}
-
-impl FromStr for PathCollection{
-    type Err = &'static str;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-       Ok(
-        PathCollection{
-         paths : s.split(":").map(|s| s.to_owned()).collect()
-        })
-    }
-}
-
-impl PathCollection {
-    fn build() -> Result<Self,&'static str> {
-        let Ok(path) = env::var("PATH") else {
-            return Err("Path not set");
-        };
-        PathCollection::from_str(&path)
-    }
-
-    fn find(&self, cmd: String) -> Option<String> {
-        for path in &self.paths {
-            let path = Path::new(&path).join(&cmd);
-            if !path.exists() {
-                continue;
-            }
-            let Ok(meta) = path.metadata() else { continue;};
-
-            if meta.permissions().mode() & 0o111 != 0 {
-                return path.to_str().map(|s| s.to_string());               
-            }            
-        }
-        None
     }
 }
 
