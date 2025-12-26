438061012727b75017af1600026f1ff4b525eb30
Axelle Ziegler <axelle-z@evidenceb.com>
2025-12-26 14:07:45 +0100

    type

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::str::FromStr;
+
+enum Command{Echo,Exit,Type}
+
+impl FromStr for Command {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "exit" => Ok(Command::Exit),
+            "echo" => Ok(Command::Echo),
+            "type" => Ok(Command::Type),
+            _ => Err("Not Found"),
+        }
+    }
+}
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
@@ -14,10 +30,22 @@ fn main() {
         let (Some(cmd),args) = (args.next(),args.collect::<Vec<&str>>())else{
             panic!("WTF!")
         };
-        match cmd {
-            "exit" => return,
-            "echo" => println!("{}",args.join(" ")),
-            c => println!("{c}: command not found")
-        }
+        if let Ok(c) = cmd.parse::<Command>(){
+            match c {
+                Command::Echo => println!("{}",args.join(" ")),
+                Command::Exit => return,
+                Command::Type => {
+                    let arg = args.first().unwrap();
+                    if arg.to_owned().parse::<Command>().is_ok(){
+                        println!("{arg} is a shell builtin");
+                    }else{
+                        println!("{arg}: not found");
+                    }
+                },
+            }
+        }else{
+            println!("{cmd}: command not found")
+        };
+        
     }
 }
