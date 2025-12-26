86a13a7d5c8fad93b9d55f0456ca6f86e6314a6b
Axelle Ziegler <axelle-z@evidenceb.com>
2025-12-26 13:32:41 +0100

    Error

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,9 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut buff = String::new();
+    let _ = io::stdin().read_line(&mut buff);
+    let command = buff.trim();
+    print!("{command}: command not found");
 }
