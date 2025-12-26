eec67a7c83755096361d6c0e65ac0554b5899306
Axelle Ziegler <axelle-z@evidenceb.com>
2025-12-26 17:41:14 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::str::FromStr;
+use std::{env, os::unix::fs::PermissionsExt, path::Path, str::FromStr};
 
 enum Command{Echo,Exit,Type}
 
@@ -17,8 +17,46 @@ impl FromStr for Command {
     }
 }
 
+struct PathCollection{
+    paths : Vec<String>
+}
+
+impl FromStr for PathCollection{
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+       Ok(
+        PathCollection{
+         paths : s.split(":").map(|s| s.to_owned()).collect()
+        })
+    }
+}
+
+impl PathCollection {
+    fn build() -> Result<Self,&'static str> {
+        let Ok(path) = env::var("PATH") else {
+            return Err("Path not set");
+        };
+        PathCollection::from_str(&path)
+    }
+
+    fn find(&self, cmd: String) -> Option<String> {
+        for path in &self.paths {
+            let path = Path::new(&path).join(&cmd);
+            if !path.exists() {
+                continue;
+            }
+            let Ok(meta) = path.metadata() else { continue;};
+
+            if meta.permissions().mode() & 0o111 != 0 {
+                return path.to_str().map(|s| s.to_string());               
+            }            
+        }
+        None
+    }
+}
+
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -39,7 +77,12 @@ fn main() {
                     if arg.to_owned().parse::<Command>().is_ok(){
                         println!("{arg} is a shell builtin");
                     }else{
-                        println!("{arg}: not found");
+                        let path = PathCollection::build().unwrap();
+                        if let Some(full_path)= path.find(arg.to_string()){
+                            println!("{arg} is {full_path}");
+                        }else{
+                            println!("{arg}: not found");
+                        }
                     }
                 },
             }
