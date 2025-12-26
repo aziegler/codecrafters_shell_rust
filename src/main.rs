b560bc6055b027c54c1be1138dff734ca16eabec
Axelle Ziegler <axelle-z@evidenceb.com>
2025-12-26 13:34:24 +0100

    REPL

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    let mut buff = String::new();
-    let _ = io::stdin().read_line(&mut buff);
-    let command = buff.trim();
-    print!("{command}: command not found");
+        let mut buff = String::new();
+        let _ = io::stdin().read_line(&mut buff);
+        let command = buff.trim();
+        println!("{command}: command not found");
+    }
 }
